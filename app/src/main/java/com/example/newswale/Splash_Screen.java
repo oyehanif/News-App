e0ff8e3644137d59bce4385bbe9237d4e7ecddbb
Hanif Shaikh <hanifshaikh8153@gmail.com>
2022-06-16 08:41:25 +0530

    Version 1.1V :tada:

--- app/src/main/java/com/example/newswale/Splash_Screen.java
+++ app/src/main/java/com/example/newswale/Splash_Screen.java
@@ -19,7 +19,7 @@ public class Splash_Screen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(Splash_Screen.this,MainActivity.class);
+                Intent intent = new Intent(Splash_Screen.this,Login.class);
                 startActivity(intent);
                 finish();
             }
